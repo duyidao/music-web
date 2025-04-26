f2ba57ebf712719ba11ad40d9bf41fc40f6827d8
duyidao <2014567296@qq.com>
2025-04-26 23:05:45 +0800

    build: 修改base url

--- vite.config.js
+++ vite.config.js
@@ -18,6 +18,7 @@ export default defineConfig({
             }
         })
     ],
+    base: 'music-web',
     server: {
         port: 6996,
     },
--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
       }
     })
   ],
+  base: 'music-web',
   server: {
     port: 6996,
   },
